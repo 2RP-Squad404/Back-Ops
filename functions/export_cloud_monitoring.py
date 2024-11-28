5681b6316d89bdb91c11c5b833a11aeb1722ef3f
Guilherme <l0rd.v4dher@gmail.com>
2024-11-28 17:38:29 -0300

    fetch: update export monitoring function

--- functions/export_cloud_monitoring.py
+++ functions/export_cloud_monitoring.py
@@ -4,14 +4,14 @@ import requests
 from google.cloud import bigquery
 from datetime import datetime, timedelta, timezone
 
-project_id = "just-lore-435816-v8"
+project_id = "integracaohomologado"
 
 end_time = (datetime.now(timezone.utc) - timedelta(hours=3)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
-url = "https://monitoring.googleapis.com/v3/projects/just-lore-435816-v8/timeSeries"
+url = "https://monitoring.googleapis.com/v3/projects/integracaohomologado/timeSeries"
 # Exportação dos bytes logs global
 params = {
-    "interval.startTime": "2024-09-17T00:00:00.000000Z", 
+    "interval.startTime": "2024-10-24T00:00:00.000000Z", 
     "interval.endTime": end_time,  
     "aggregation.alignmentPeriod": "60s",
     "aggregation.perSeriesAligner": "ALIGN_SUM", 
@@ -36,12 +36,12 @@ response = requests.get(url, headers=headers, params=params)
 data = response.json()
 
 # Se 'data' não é uma lista, você pode precisar convertê-lo em uma lista.
-if not isinstance(data, list):
+if isinstance(data, dict):
     data = [data]  
 
 client = bigquery.Client(project=project_id)
 
-dataset_id = 'metrics'
+dataset_id = 'billing'
 table_id = 'byte_count_global'
 table_ref = client.dataset(dataset_id).table(table_id)
 
@@ -57,15 +57,16 @@ load_job = client.load_table_from_json(data, table_ref, job_config=job_config)
 # Aguarda a conclusão
 load_job.result()
 
+#----------------------------------------------------------------------------------------------------------
 # Exportação dos bytes logs dataset
 params = {
-    "interval.startTime": "2024-09-17T00:00:00.000000Z", 
+    "interval.startTime": "2024-10-24T00:00:00.000000Z", 
     "interval.endTime": end_time,  
     "aggregation.alignmentPeriod": "60s",
     "aggregation.perSeriesAligner": "ALIGN_SUM", 
     "aggregation.crossSeriesReducer": "REDUCE_SUM",  
     "filter": 'metric.type="logging.googleapis.com/byte_count" resource.type="bigquery_dataset"',
-    "aggregation.groupByFields": "metric.labels.key"
+    "aggregation.groupByFields": "resource.label.\"dataset_id\""
 }
 
 # Requisição HTTP
@@ -75,7 +76,7 @@ response = requests.get(url, headers=headers, params=params)
 data = response.json()
 
 # Se 'data' não é uma lista, você pode precisar convertê-lo em uma lista.
-if not isinstance(data, list):
+if isinstance(data, dict):
     data = [data]  
 
 table_id = 'byte_count_dataset'
@@ -87,15 +88,16 @@ load_job = client.load_table_from_json(data, table_ref, job_config=job_config)
 # Aguarda a conclusão
 load_job.result()
 
-# Exportação dos bytes logs routine
+#----------------------------------------------------------------------------------------------------------
+# Exportação dos bytes logs severity
 params = {
-    "interval.startTime": "2024-09-17T00:00:00.000000Z", 
-    "interval.endTime": end_time,  
+    "interval.startTime": "2024-10-24T00:00:00.000000Z",
+    "interval.endTime": end_time,
     "aggregation.alignmentPeriod": "60s",
-    "aggregation.perSeriesAligner": "ALIGN_SUM", 
-    "aggregation.crossSeriesReducer": "REDUCE_SUM",  
-    "filter": 'metric.type="logging.googleapis.com/byte_count" resource.type="dataform.googleapis.com/Repository"',
-    "aggregation.groupByFields": "metric.labels.key"
+    "aggregation.perSeriesAligner": "ALIGN_SUM",
+    "aggregation.crossSeriesReducer": "REDUCE_SUM",
+    "filter": 'metric.type="logging.googleapis.com/log_entry_count" resource.type="global"',
+    "aggregation.groupByFields": "metric.label.severity"
 }
 
 # Requisição HTTP
@@ -104,11 +106,40 @@ response = requests.get(url, headers=headers, params=params)
 # Carrega a resposta JSON
 data = response.json()
 
-# Se 'data' não é uma lista, você pode precisar convertê-lo em uma lista.
-if not isinstance(data, list):
+if isinstance(data, dict):
     data = [data]  
 
-table_id = 'byte_count_routine'
+table_id = 'log_severity'
+table_ref = client.dataset(dataset_id).table(table_id)
+
+# Exporta para a tabela
+load_job = client.load_table_from_json(data, table_ref, job_config=job_config)
+
+# Aguarda a conclusão
+load_job.result()
+
+#----------------------------------------------------------------------------------------------------------
+# Exportação dos bytes logs do Dataform
+params = {
+    "interval.startTime": "2024-10-24T00:00:00.000000Z",
+    "interval.endTime": end_time,
+    "aggregation.alignmentPeriod": "60s",
+    "aggregation.perSeriesAligner": "ALIGN_SUM",
+    "aggregation.crossSeriesReducer": "REDUCE_SUM",
+    "filter": 'metric.type="logging.googleapis.com/byte_count" resource.type="dataform.googleapis.com/Repository"',
+    "aggregation.groupByFields": "resource.label.repository_id"
+}
+
+# Requisição HTTP
+response = requests.get(url, headers=headers, params=params)
+
+# Carrega a resposta JSON
+data = response.json()
+
+if isinstance(data, dict):
+    data = [data]  
+
+table_id = 'byte_count_dataform'
 table_ref = client.dataset(dataset_id).table(table_id)
 
 # Exporta para a tabela
